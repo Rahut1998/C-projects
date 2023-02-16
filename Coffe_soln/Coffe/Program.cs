0d59e4c407fc73d4e1cd898db104ba667b642a92
Rahut1998 <a.rahutanavar@accenture.com>
2023-02-16 09:31:12 +0530

    2nd

--- Coffe_soln/Coffe/Program.cs
+++ Coffe_soln/Coffe/Program.cs
@@ -40,5 +40,7 @@ class coffee
         Console.WriteLine("Thanks for shopping with us have nice day ahead");
         Console.WriteLine("Please visit once again");
         Console.WriteLine("Please visit once again");
+        Console.WriteLine("Thank you visit again");
+        Console.WriteLine("Thank you visit again");
     }
 }
